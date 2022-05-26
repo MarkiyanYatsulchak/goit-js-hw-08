180dc18f3acb15b6bae550f00225d522ea9fafde
MarkiyanYatsulchak <m.yatsulchak@gmail.com>
2022-05-26 21:09:27 +0300

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,17 @@
+import throttle from 'lodash.throttle';
 
-    const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
 
-    player.on('play', function() {
-        console.log('played the video!')
-    });
+const player = new Vimeo.Player(iframe);
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title)
-    });
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+if (localStorage.getItem(STORAGE_KEY)) {
+  player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+
+window.addEventListener('DOMContentLoaded', setFormValue);
+
+const KEY = 'feedback-form-state';
+
+const state = localStorage.getItem(KEY) ? JSON.parse(localStorage.getItem(KEY)) : {};
+
+feedbackForm.addEventListener('submit', evt => {
+  evt.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  evt.currentTarget.reset();
+  localStorage.removeItem(KEY);
+});
+
+email.addEventListener('input', throttle(handleTextInput, 500));
+
+message.addEventListener('input', throttle(handleTextInput, 500));
+
+function handleTextInput(evt) {
+  state[evt.target.name] = evt.target.value;
+  localStorage.setItem(KEY, JSON.stringify(state));
+}
+
+function setFormValue() {
+  if (!localStorage.getItem(KEY)) {
+    return;
+  }
+  const savedFormData = JSON.parse(localStorage.getItem(KEY));
+  if (savedFormData.email) {
+    email.value = savedFormData.email;
+  }
+  if (savedFormData.message) {
+    message.value = savedFormData.message;
+  }
+}
